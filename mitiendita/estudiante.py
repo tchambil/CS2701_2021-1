6f9c81406e82fe010c8469eaa039d6ba3cb9ed14
tchambil <antony_epis@hotmail.com>
2021-06-11 20:00:35 -0500

    add template

---
+++ mitiendita/estudiante.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from config import *
+
+app = Flask(__name__)
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        nombrEstudent = request.form['nombre']
+
+        try:
+
+            result =get_estudiante_by_NombreSeguro(nombrEstudent)
+
+            return render_template('index.html', estudiantes=result)
+        except:
+            return 'Ocurrío un problema al buscar'
+
+    else:
+        result = get_estudiante_All()
+        return render_template('index.html', estudiantes=result)
+def get_estudiante_by_Nombre(nombre):
+    db, cur = get_connection()
+    print(nombre)
+    cur.execute('select * from estudiante where nombres ='+"'"+nombre+"'")
+    print(cur)
+    return cur.fetchall()
+
+def get_estudiante_by_NombreSeguro(nombre):
+    db, cur = get_connection()
+    print(nombre)
+    cur.execute('prepare myplan as select * from estudiante where nombres like $1;')
+    cur.execute('execute myplan (%s)', (nombre,))
+    return cur.fetchall()
+
+def get_estudiante_All():
+    conn, cur = get_connection()
+    cur.execute('select * from estudiante')
+    return cur.fetchall()
+
+
+if __name__ == '__main__':
+    app.run()
